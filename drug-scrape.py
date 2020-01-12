19a9d56d16157939f718a01a68807904e51e01a7
Weber Liu <wliu1996@hotmail.com.au>
2020-01-13 01:12:48 +1100

    added functionality for creating cloze cards from amh for ANKI

--- drug-scrape.py
+++ drug-scrape.py
@@ -1,17 +1,140 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import details #importing from details.py in same folder containing email/password
 
+#genanki stuff
+import sys
+from genanki import Model
+from genanki import Note
+from genanki import Deck
+from genanki import Package
+
+CSS = """.card {
+ font-family: arial;
+ font-size: 20px;
+ text-align: center;
+ color: black;
+ background-color: white;
+}
+.cloze {
+ font-weight: bold;
+ color: blue;
+}
+.nightMode .cloze {
+ color: lightblue;
+}
+"""
+MY_CLOZE_MODEL = Model(
+  998877661,
+  'My Cloze Model',
+  fields=[
+    {'name': 'Text'},
+    {'name': 'Extra'},
+  ],
+  templates=[{
+    'name': 'My Cloze Card',
+    'qfmt': '{{cloze:Text}}',
+    'afmt': '{{cloze:Text}}<br>{{Extra}}',
+  },],
+  css=CSS,
+  model_type=Model.CLOZE)
+
+notes = []
+
+#begin webdriver
 driver = webdriver.Chrome(executable_path="./chromedriver.exe") #??
 
 driver.get("https://amhonline-amh-net-au.ezproxy1.library.usyd.edu.au/drugs/monographs")
-time.sleep(10);
+time.sleep(2);
+#click accept button on uniKey paywall page
+driver.find_element_by_class_name('accept').click();
+time.sleep(2);
 
-time.sleep(15);
+#fill in login details and submit form for login
+driver.find_element_by_id('userNameInput').send_keys(details.email)
+driver.find_element_by_id('passwordInput').send_keys(details.password)
+driver.find_element_by_id('submitButton').click();
+time.sleep(5);
 
-content = driver.find_elements_by_class_name('browse-by')
-for i in range(0, 25):
-  print(content[i])
+contentList = driver.find_elements_by_class_name('index')[6].find_elements_by_tag_name('a')
+
+# Fetch and store the links in object links[]
+links = []
+for item in contentList:
+    links.append(item.get_attribute('href'))
+
+for i in range(0, 3):
+  link = links[i]
+  driver.get(link)
+  # time.sleep(1)
+  print(link)
+  # title = driver.find_elements_by_tag_name('header')[1].find_elements_by_tag_name('h1')[0].text
+  # MOA = driver.get_elements_by_class_name('mode-action')[1].text
+  # indication = driver.get_elements_by_class_name('indication')[1].text
+  # adv_eff = driver.get_elements_by_class_name('adv-eff')[1].text
+  # dosage = driver.get_elements_by_class_name('dosage')[1].text
+  # prac_pts = driver.get_elements_by_class_name('prac-pts')[1].text
+  field = "'"
+  try:
+    title = driver.find_elements_by_tag_name('header')[1].find_elements_by_tag_name('h1')[0].text
+    field += title + ": <br /> ";
+  except:
+    print("Title not found")
+
+  field += "Link: " + link + " <br /> "
+  try:
+    MOA = driver.find_elements_by_class_name('mode-action')[1].text.replace('\n', ':').replace('\r', ';;')
+    field += "{{c1::MOA: " + MOA + "}} <br /> "
+  except:
+    print("MOA not found")
+
+  try:
+    indication = driver.find_elements_by_class_name('indication')[1].text.replace('\n', ':').replace('\r', ';;')
+    field += "{{c2::Indication: " + indication + "}} <br /> "
+  except:
+    print("Indication not found")
+
+  try:
+    adv_eff = driver.find_elements_by_class_name('adv-eff')[1].text.replace('\n', ':').replace('\r', ';;')
+    field += "{{c3::Adverse effects: " + adv_eff + "}} <br /> "
+  except:
+    print("adverse effects not found")
+
+  try:
+    dosage = driver.find_elements_by_class_name('dosage')[1].text.replace('\n', ':').replace('\r', ';;')
+    field += "{{c4::Dosage: " + dosage + "}} <br /> "
+  except:
+    print("dosage not found")
+    
+  try:
+    prac_pts = driver.find_elements_by_class_name('prac-pts')[1].text.replace('\n', ':').replace('\r', ';;')
+    field += "{{c5::Practical points: " + prac_pts + "}}"
+  except:
+    print("practical points not found")
+  
+  field += "'"
+  fields = [field, ' ']
+  # # Question: NOTE TWO: [...]              2nd deletion     3rd deletion
+  # # Answer:   NOTE TWO: **1st deletion**   2nd deletion     3rd deletion
+  # #
+  # # Question: NOTE TWO: 1st deletion       [...]            3rd deletion
+  # # Answer:   NOTE TWO: 1st deletion     **2nd deletion**   3rd deletion
+  # #
+  # # Question: NOTE TWO: 1st deletion       2nd deletion     [...]
+  # # Answer:   NOTE TWO: 1st deletion       2nd deletion   **3rd deletion**
+  # fields = ['NOTE TWO: {{c1::1st deletion}} {{c2::2nd deletion}} {{c3::3rd deletion}}', '']
+  my_cloze_note = Note(model=MY_CLOZE_MODEL, fields=fields)
+  notes.append(my_cloze_note)
+
+deckname = 'AMH_drugs'
+deck = Deck(deck_id=0, name=deckname)
+for note in notes:
+  deck.add_note(note)
+fout_anki = '{NAME}.apkg'.format(NAME=deckname)
+Package(deck).write_to_file(fout_anki)
+print('{N} Notes WROTE: {APKG}'.format(N=len(notes), APKG=fout_anki))
+
 
 
 
